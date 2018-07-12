895d5e991dcc014af8a4b36ffb86a902da8a1a9a
cot Enterprise <chatthong.r@mail.kmutt.ac.th>
2018-07-13 00:36:16 +0200

    mockup api

--- SmartMorn-BackEnd/src/routes/users.js
+++ SmartMorn-BackEnd/src/routes/users.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
-import { login, register } from '../controllers/auth';
+import { login, register} from '../controllers/auth';
+import { graph, pie1, pie2} from '../controllers/dashboard.api'
 
 const router = Router();
 
 //router.get('/', auth);
 router.post('/login',login);
 router.post('/register',register);
+router.get('/graph',graph);
+router.get('/pie1',pie1);
+router.get('/pie2',pie2);
 // router.get('/info',(req,res) => {
 // 	res.send('info');
 // });
